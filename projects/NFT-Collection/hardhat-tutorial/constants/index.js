2112ea2649dfc84246b2540eaf06d4fb05e83218
bcode <116942079+devbfactor@users.noreply.github.com>
2022-12-18 16:55:35 +0800

    commit

---
+++ projects/NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x7C6070cF5aC5213fa740Ea0370EFc930cb605A25";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
